60b7bd153ebea71c41dd105b9deaffd839d332fb
dan-rhul <example@example.com>
2021-01-26 16:48:16 +0000

    perhaps

--- src/main/java/org/example/rhulproject/Project.java
+++ src/main/java/org/example/rhulproject/Project.java
@@ -1,11 +1,12 @@
 package org.example.rhulproject;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.io.IOException;
-import java.time.Instant;
-
+@Controller
+@SpringBootApplication
 public class Project {
     public static void main(String[] args) {
         SpringApplication.run(Project.class, args);
@@ -15,23 +16,4 @@ public class Project {
     String index() {
         return "index";
     }
-//    private final Express app;
-//
-//    public Project(Express app) throws IOException {
-//        this.app = app;
-//
-//        registerRoutes();
-//        startServer();
-//    }
-//
-//    public static void main(String[] args) {}
-//
-//    private void registerRoutes() throws IOException {
-//        this.app.get("/", Middleware.statics("pages/"));
-//        this.app.get("/time", (request, response) -> response.send(Instant.now().toString()));
-//    }
-//
-//    private void startServer() {
-//        this.app.listen(Integer.parseInt(System.getenv("PORT")));
-//    }
 }
