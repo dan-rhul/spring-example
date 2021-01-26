26ebf24c33e02eb2072a9a9b54814c99dd9b0755
dan-rhul <example@example.com>
2021-01-26 16:39:41 +0000

    perhaps

--- src/main/java/org/example/rhulproject/Project.java
+++ src/main/java/org/example/rhulproject/Project.java
@@ -1,29 +1,37 @@
 package org.example.rhulproject;
 
-import express.Express;
-import express.middleware.Middleware;
+import org.springframework.boot.SpringApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.io.IOException;
 import java.time.Instant;
 
 public class Project {
-    private final Express app;
-
-    public Project(Express app) throws IOException {
-        this.app = app;
-
-        registerRoutes();
-        startServer();
+    public static void main(String[] args) {
+        SpringApplication.run(Project.class, args);
     }
 
-    public static void main(String[] args) {}
-
-    private void registerRoutes() throws IOException {
-        this.app.get("/", Middleware.statics("pages/"));
-        this.app.get("/time", (request, response) -> response.send(Instant.now().toString()));
+    @RequestMapping("/")
+    String index() {
+        return "index";
     }
-
-    private void startServer() {
-        this.app.listen(Integer.parseInt(System.getenv("PORT")));
-    }
+//    private final Express app;
+//
+//    public Project(Express app) throws IOException {
+//        this.app = app;
+//
+//        registerRoutes();
+//        startServer();
+//    }
+//
+//    public static void main(String[] args) {}
+//
+//    private void registerRoutes() throws IOException {
+//        this.app.get("/", Middleware.statics("pages/"));
+//        this.app.get("/time", (request, response) -> response.send(Instant.now().toString()));
+//    }
+//
+//    private void startServer() {
+//        this.app.listen(Integer.parseInt(System.getenv("PORT")));
+//    }
 }
