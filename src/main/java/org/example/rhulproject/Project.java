6fcd6f5b4aeb2215b7580ea7d78ff1c36d52dfd1
dan-rhul <example@example.com>
2021-01-26 16:59:14 +0000

    perhaps

--- src/main/java/org/example/rhulproject/Project.java
+++ src/main/java/org/example/rhulproject/Project.java
@@ -3,17 +3,27 @@ package org.example.rhulproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
 
 @Controller
 @SpringBootApplication
+@RestController
 public class Project {
     public static void main(String[] args) {
         SpringApplication.run(Project.class, args);
     }
 
-    @RequestMapping("/")
+    @GetMapping("/")
     String index() {
         return "index";
+    }
+
+    @GetMapping("/time")
+    String time() {
+        return Instant.now().toString();
     }
 }
